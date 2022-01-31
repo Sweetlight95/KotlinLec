0b81e1b193c882c138c6e604e59ec8b90e369b99
Sweetlight95 <princessadesuwa93@gmail.com>
2022-01-31 15:58:05 +0100

    new

---
+++ classFour.kt
@@ -0,0 +1,21 @@
+//logical operators
+//fun main() {
+    //val a = 10
+    //val b = 9
+  //  val c = -1 
+//    val result: Boolean
+
+     // result = (a>b) and (a>c)
+   // result = (a>b) && (a>c)
+ //   println(result)
+//}
+
+//in Operators
+fun main() {
+    val numbers = intArrayOf(1, 42, -3)
+    if(4 in numbers){
+        println("numbers array contains 4.")
+    }else {
+        println("numbers array contains no 4.")
+    }
+}
---
+++ classThree.kt
@@ -0,0 +1,22 @@
+//fun main() {
+  //  var oneNumber = 60
+//    var twoNumber = 20
+
+    //if (oneNumber > twoNumber) {
+   //     println("One letter is greater")
+  //  } else {
+//        println("Two letter is lesser")
+  //  }
+//}
+fun main() {
+    val a = -12
+    val b = 12
+    val max = if(a > b) {
+        println("a is larger than b")
+        a
+    } else {
+        println("b is lesser than a")
+        b
+    }
+    println("max = $max")
+}
