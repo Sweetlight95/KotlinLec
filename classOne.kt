56acd8bcbc253e4abea969b373d28debbdaede4f
Sweetlight95 <princessadesuwa93@gmail.com>
2022-01-31 15:22:38 +0100

    new

---
+++ classOne.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Hello World")
+
+    val name = "seyi"
+    var score = 45
+    println(name)
+    println(score)
+
+    score = 100
+    println(score)
+
+    score = 5 + 8
+    println(score)
+}
+
